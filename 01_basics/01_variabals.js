7848effa98de3568270ae27e2814393a274240cc
Luckydadhich <152679870+Luckydadhich@users.noreply.github.com>
2026-07-30 11:26:40 +0000

    save1.1

---
+++ 01_basics/01_variabals.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail = "test@test.com"
+var accountPass = "123456"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 12
+
+// console.log(accountId);
+
+
+
+accountEmail = "test@test"
+accountPass = "123"
+accountCity = "delhi"
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
