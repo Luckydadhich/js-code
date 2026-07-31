031847365166354efc19249b5cc8816f529cd1f3
Luckydadhich <152679870+Luckydadhich@users.noreply.github.com>
2026-07-31 09:14:27 +0000

    conveop

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "test"
+
+// console.log(typeof score)
+
+let valueInNuber = Number(score)
+// console.log(valueInNuber)
+
+// "33" = 33
+// "33abc" = NaN
+
+
+let isLoggedIn = "test"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
